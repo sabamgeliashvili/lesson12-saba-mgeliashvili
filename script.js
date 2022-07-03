2d4bcd8a550df3778432d718058ac879c6fb5ecd
sabamgeliashvili <sba69717@gmail.com>
2022-07-03 23:36:17 +0400

    .

--- script.js
+++ script.js
@@ -1,65 +1,65 @@
-// // variables
-// let array = [ 'watermelon', 'pear', 10, 45, 50, 'apple', 'ananas' ] ;
-// console.log(array[0])
-// console.log(array[1])
-// console.log(array[5])
-// console.log(array[6])
+// variables
+let array = [ 'watermelon', 'pear', 10, 45, 50, 'apple', 'ananas' ] ;
+console.log(array[0])
+console.log(array[1])
+console.log(array[5])
+console.log(array[6])
 
-// // 2
-
-
-// let array = [32, 10, 'hello', null, 'hello2', 50 ];
-// for(let item of array){
-// if (item % 5==0){
-// console.log(item);
-// }
-// }
+// 2
 
 
-// // // 3
-// let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
-// for(let item of numbers){
-//     if (item>5) {
-//     console.log(item)    
-//     }
-// }
+let array = [32, 10, 'hello', null, 'hello2', 50 ];
+for(let item of array){
+if (item % 5==0){
+console.log(item);
+}
+}
 
-// // 4
 
-// let  user = {
-//     firstname: 'giorgi',
-//     lastname: 'smith',
-//      age: 25,
-//    studentstatus: 'active',
-//    };
-//    console.log(user.studentstatus);
+// // 3
+let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+for(let item of numbers){
+    if (item>5) {
+    console.log(item)    
+    }
+}
+
+// 4
+
+let  user = {
+    firstname: 'giorgi',
+    lastname: 'smith',
+     age: 25,
+   studentstatus: 'active',
+   };
+   console.log(user.studentstatus);
 
 
    
 
-// // 5
+// 5
 
-// let pets = ['dog','cat','lion','tiger','monkey'];
-// for(let x=0; x < pets.length; x++){
-//     console.log(pets[x]);
-// }
+let pets = ['dog','cat','lion','tiger','monkey'];
+for(let x=0; x < pets.length; x++){
+    console.log(pets[x]);
+}
 
 
-// 6
-// let user = {
-// 	name: 'giorgi',
-// 	age:  20,
-// 	studentstatus: 'active',
-// };
-// if(user.age > 18, user.studentstatus ='active'){
-// console.log('hello');
-// } if(user.name='giorgi'){
-//     console.log('hello giorgi');
-// }if(user.studentstatus= 'active', user.age < 25){
-// console.log('hello world');
-// } else{
-//     console.log('error');
-// }
+6
+let user = {
+	name: 'giorgi',
+	age:  20,
+	studentstatus: 'active',
+};
+if(user.age > 18, user.studentstatus ='active'){
+console.log('hello');
+} if(user.name='giorgi'){
+    console.log('hello giorgi');
+}if(user.studentstatus= 'active', user.age < 25){
+console.log('hello world');
+} else{
+    console.log('error');
+}
 
 
 
