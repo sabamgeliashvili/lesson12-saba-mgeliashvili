861da3bf013f3fae39e75b5c7d9ab4fc8784a8f2
sabamgeliashvili <sba69717@gmail.com>
2022-07-03 21:09:52 +0400

    add js

---
+++ script.js
@@ -0,0 +1,64 @@
+// variables
+let array = [ 'watermelon', 'pear', 10, 45, 50, 'apple', 'ananas' ] ;
+console.log(array[0])
+console.log(array[1])
+console.log(array[5])
+console.log(array[6])
+
+// 2
+
+
+let array = [32, 10, 'hello', null, 'hello2', 50 ]
+for(let item of numbers){
+if(item % 5 ==0){
+console.log(item);
+}
+}
+
+
+// 3
+let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+for(let item of numbers){
+    if (item>5) {
+    console.log(item)    
+    }
+}
+
+// 4
+
+let  user = {
+    firstname: 'giorgi',
+    lastname: 'smith',
+     age: 25,
+   studentstatus: 'active',
+   };
+   console.log(user.studentstatus)
+
+   
+
+// 5
+
+let pets['dog','cat','lion','tiger','monkey'];
+for(let x=0; x < pets.lenght; x++){
+    console.log(pets[x])
+}
+
+
+// 6
+let user = {
+	name: 'giorgi',
+	age:  20,
+	studentstatus: 'active',
+}
+if(user.age>18 & user.studentstatus ='active'){
+console.log(hello)
+} else if(user.name=giorgi){
+    console.log('hello giorgi')
+} else if(user.studentstatus='active' & user.age<25){
+console.log('hello world')
+} else{
+    console.log('error')
+}
+
+
+   
